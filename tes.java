38d3ca968838999a3fec163833f9155527032013
Raihan-Ardhanis <raihan_raharja_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:13:42 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,6 @@
+public class tes {
+    public static void main(String[] args)
+        String nama = "Raihan";
+        String kelas = "X RPL 5";
+        System.out.print("Halo, nama saya " + nama  + " dari kelas " + kelas );
+        }   
