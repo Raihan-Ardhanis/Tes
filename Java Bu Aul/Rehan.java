d893ab3717a35b46ae723239654eac015f5c10f0
Raihan-Ardhanis <raihan_raharja_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:18:28 +0700

    Add files via upload

---
+++ Java Bu Aul/Rehan.java
@@ -0,0 +1,15 @@
+public class Rehan{
+    public static void main(String[] args) {
+        String nama = "Raihan";
+        String alamat = "Pandaan";
+        int usia=15;
+        char jenisKelamin='L';
+        double beratBadan=75.00;
+   
+        System.out.println("Nama: "+ nama);
+        System.out.println("Alamat: "+ alamat);
+        System.out.println("Usia: "+ usia);
+        System.out.println("Jenis Kelamin: "+ jenisKelamin);
+        System.out.println("Berat Badan"+beratBadan);
+    }
+}
